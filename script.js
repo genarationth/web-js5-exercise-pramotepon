7c7edb30e9a7de746ffe4d7347cd3d1eadfb3f76
Zeekerz <pramote.p.on@gmail.com>
2023-03-15 17:12:44 +0700

    Exercise #2

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ console.log(peoples); // ['Sofia', 'David', 'Juan', 'Sara', 'Augustin']
 // Part 3
 peoples.splice(2,0,'Elena');
 console.log(peoples); // ['Sofia', 'David', 'Elena', 'Juan', 'Sara', 'Augustin']
+
+/* 
+    Exercise #2
+*/
+let stars = '';
+for (let i = 0; i < 5; i++) {
+    stars += '*';
+    console.log(stars);
+    
+}
